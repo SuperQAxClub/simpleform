39ba35e19d752067ce3905d29ce309d96e717039
Quang Anh <nguyenquanganh38@gmail.com>
2021-03-23 09:30:32 +0700

    Fix another typo

--- simple.js
+++ simple.js
@@ -1,7 +1,7 @@
 $(document).on('submit','#loginForm',function(e){
     e.preventDefault();
-    username = $('input[name="'+txtUserName+'"]').val();
-    password = $('input[name="'+txtPassword+'"]').val();
+    username = $('input[name="txtUserName"]').val();
+    password = $('input[name="txtPassword"]').val();
     if(username == "admin" && password == "admin") {
         alert("Đăng nhập thành công");
     } else {
