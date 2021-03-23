727a15c7500e7108fd24c5eee6552f29294de3e6
Eugene Nguyen <eugenenguyen314@gmail.com>
2021-03-23 09:36:52 +0700

    Success redirect v2

--- simple.js
+++ simple.js
@@ -3,7 +3,7 @@ $(document).on('submit','#loginForm',function(e){
     username = $('input[name="txtUserName"]').val();
     password = $('input[name="txtPassword"]').val();
     if(username == "admin" && password == "admin") {
-        location.href("success.html")
+        windows.location.href="success.html";
     } else {
         alert("Đăng nhập thất bại");
     }
